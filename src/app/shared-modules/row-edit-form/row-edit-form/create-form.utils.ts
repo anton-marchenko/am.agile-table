bbb309a9f3a632667461dd943ac8ef9d8cca6c60
anton-marchenko <fattan@ya.ru>
2021-10-31 11:34:49 +0300

    Refactor createForm utils

---
+++ src/app/shared-modules/row-edit-form/row-edit-form/create-form.utils.ts
@@ -0,0 +1,87 @@
+import { FormControl, FormGroup } from '@angular/forms';
+import { GridColumn, Row } from '@shared/models/table';
+
+export const createForm = (
+  row: Row | null,
+  columns: ReadonlyArray<GridColumn>,
+) => {
+  const acc: {
+    explicit: { [key: string]: FormControl };
+    attributed: {
+      text: { [key: number]: FormControl };
+      date: { [key: number]: FormControl };
+      multiList: { [key: number]: FormControl };
+    };
+  } = {
+    explicit: {},
+    attributed: { text: {}, date: {}, multiList: {} },
+  } as const;
+  const cfg = columns.reduce((acc, col) => {
+    if (col.kind === 'explicit') {
+      return {
+        ...acc,
+        explicit: {
+          ...acc.explicit,
+          [col.alias]: new FormControl(row ? col.resolveFormValue(row) : null),
+        },
+      };
+    } else {
+      switch (col.cellType) {
+        case 'text': {
+          return {
+            ...acc,
+            attributed: {
+              ...acc.attributed,
+              text: {
+                ...acc.attributed.text,
+                [col.attributeId]: new FormControl(
+                  row ? col.resolveFormValue(row) : null,
+                ),
+              },
+            },
+          };
+        }
+        case 'date': {
+          return {
+            ...acc,
+            attributed: {
+              ...acc.attributed,
+              date: {
+                ...acc.attributed.date,
+                [col.attributeId]: new FormControl(
+                  row ? col.resolveFormValue(row) : null,
+                ),
+              },
+            },
+          };
+        }
+        case 'multiList': {
+          return {
+            ...acc,
+            attributed: {
+              ...acc.attributed,
+              multiList: {
+                ...acc.attributed.multiList,
+                [col.attributeId]: new FormControl(
+                  row ? col.resolveFormValue(row) : null,
+                ),
+              },
+            },
+          };
+        }
+      }
+    }
+  }, acc);
+
+  const form = new FormGroup({
+    rowId: new FormControl(row?.rowId ?? null),
+    explicit: new FormGroup(cfg.explicit),
+    attributed: new FormGroup({
+      text: new FormGroup(cfg.attributed.text),
+      date: new FormGroup(cfg.attributed.date),
+      multiList: new FormGroup(cfg.attributed.multiList),
+    }),
+  });
+
+  return form;
+};
--- src/app/shared-modules/row-edit-form/row-edit-form/row-edit-form.component.ts
+++ src/app/shared-modules/row-edit-form/row-edit-form/row-edit-form.component.ts
@@ -7,6 +7,7 @@ import {
   EventEmitter,
 } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { createForm } from '@shared-modules/row-edit-form/row-edit-form/create-form.utils';
 import { ProcessingState, ProcessSubj } from '@shared/models/processing-state';
 import { State } from '@shared/models/response-state';
 import { GridColumn } from '@shared/models/table';
@@ -17,88 +18,6 @@ import { trackByFn } from '@shared/utils/track-by.utils';
 import { unwrapNullable } from '@shared/utils/unwrap-nullable';
 import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
-
-const createForm = (row: Row | null, columns: ReadonlyArray<GridColumn>) => {
-  const acc: {
-    explicit: { [key: string]: FormControl };
-    attributed: {
-      text: { [key: number]: FormControl };
-      date: { [key: number]: FormControl };
-      multiList: { [key: number]: FormControl };
-    };
-  } = {
-    explicit: {},
-    attributed: { text: {}, date: {}, multiList: {} },
-  } as const;
-  const cfg = columns.reduce((acc, col) => {
-    if (col.kind === 'explicit') {
-      return {
-        ...acc,
-        explicit: {
-          ...acc.explicit,
-          [col.alias]: new FormControl(row ? col.resolveFormValue(row) : null),
-        },
-      };
-    } else {
-      switch (col.cellType) {
-        case 'text': {
-          return {
-            ...acc,
-            attributed: {
-              ...acc.attributed,
-              text: {
-                ...acc.attributed.text,
-                [col.attributeId]: new FormControl(
-                  row ? col.resolveFormValue(row) : null,
-                ),
-              },
-            },
-          };
-        }
-        case 'date': {
-          return {
-            ...acc,
-            attributed: {
-              ...acc.attributed,
-              date: {
-                ...acc.attributed.date,
-                [col.attributeId]: new FormControl(
-                  row ? col.resolveFormValue(row) : null,
-                ),
-              },
-            },
-          };
-        }
-        case 'multiList': {
-          return {
-            ...acc,
-            attributed: {
-              ...acc.attributed,
-              multiList: {
-                ...acc.attributed.multiList,
-                [col.attributeId]: new FormControl(
-                  row ? col.resolveFormValue(row) : null,
-                ),
-              },
-            },
-          };
-        }
-      }
-    }
-  }, acc);
-
-  const form = new FormGroup({
-    rowId: new FormControl(row?.rowId ?? null),
-    explicit: new FormGroup(cfg.explicit),
-    attributed: new FormGroup({
-      text: new FormGroup(cfg.attributed.text),
-      date: new FormGroup(cfg.attributed.date),
-      multiList: new FormGroup(cfg.attributed.multiList),
-    }),
-  });
-
-  return form;
-};
 
 @Component({
   selector: 'am-row-edit-form',
